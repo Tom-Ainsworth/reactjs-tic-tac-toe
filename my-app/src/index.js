0487881796eb691cba0b6b04e2b0c3521a6027a1
Tom Ainsworth <tomainsworth94@gmail.com>
2022-05-07 15:17:14 +0100

    Add turn taking functionality

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,26 +15,31 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
     // () needed after return so that JS doesn't automatically add a ; after it, which would break the code.
-    return (<Square 
-              value={this.state.squares[i]}
-              onClick={() => this.handleClick(i)}
-    />
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
     );
   }
   
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
